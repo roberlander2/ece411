8ee9417c64ef9947ec3927c6374b962c005bc40f
Sahil Shah <sahils2@illinois.edu>
2019-11-16 23:28:10 -0600

    create parameters and io ports for BTB

--- hdl/btb.sv
+++ hdl/btb.sv
@@ -1,8 +1,18 @@
-module BTB(
+import rv32i_types::*;
 
-
-
+module BTB #(parameter size = 10)(
+	input logic clk, 
+	input logic [size-1:0] gshare_idx,
+	input load,
+	input read,
+	input rv32i_word wtarget,
+	output rv32i_word rtarget
 );
 
+logic tag = gshare_idx[9:5];
+logic idx = gshare_idx[4:0];
+
+//instantiate data arrays (for target data) 
+//and arrays for tag and valid data
 
 endmodule: BTB
